698108dcfb27462fc3c170d4f051bd6aa2e58d6f
Alec Sands <sandsalecv@gmail.com>
2020-05-27 10:36:09 -0500

    setting up basic routing

--- react-tdd/src/App.js
+++ react-tdd/src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    <Switch>
+      <Route path="/about">
+        <p>About</p>
+      </Route>
+      <Route path="/users">
+        <p>Users</p>
+      </Route>
+      <Route path="/">
+        <p>Home</p>
+      </Route>
+    </Switch>
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. TDD :D React App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload. TDD :D React App
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
